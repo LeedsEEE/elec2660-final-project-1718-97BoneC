2c3e617f4f59dfcd8711d7ad37685a1c94943168
Timothy Bone <t.bone@fpe-ltd.co.uk>
2017-12-07 00:48:58 +0000

    Added activity indicator to inform user of background progress when loading catalogues

--- FPESeals/Catalogue/WebViewController.m
+++ FPESeals/Catalogue/WebViewController.m
@@ -9,8 +9,10 @@
 #import "WebViewController.h"
 
 
-@interface WebViewController () <UIWebViewDelegate,WKUIDelegate>
+@interface WebViewController () <WKUIDelegate, WKNavigationDelegate>
 @property (strong, nonatomic) IBOutlet WKWebView *PDFViewer;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *loadingIndicator;
+
 
 @end
 
@@ -19,28 +21,57 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    [self intialiseWebViewer];
+    [self intialiseWebViewer];      // WKWebView
     
-    NSLog(@"%@", self.catalogueURLString);
+    [self createURLRequest];        // NSURL Request
+    
+    [self loadingProgress];         // Activity Indicator
+}
+
+- (void)didReceiveMemoryWarning {   [super didReceiveMemoryWarning];    }
+
+- (void)intialiseWebViewer
+{
+    // Generating WKWebView
+    // Initialising with size of view (device)
+    self.PDFViewer = [[WKWebView alloc] initWithFrame:self.view.bounds];
+    
+    // Setting Delegates
+    self.PDFViewer.UIDelegate = self;
+    self.PDFViewer.navigationDelegate = self;
+    
+    // Adding View
+    [self.view addSubview:self.PDFViewer];
+    
+    // Advice from: https://stackoverflow.com/questions/30737254/wkwebview-added-as-subview-is-not-resized-on-rotation-in-swift
+    [self.PDFViewer setAutoresizingMask:UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth];
+}
+
+
+- (void)createURLRequest
+{
+    // Getting Catalogue from website (URL)
     NSURL *catalogueURL = [NSURL URLWithString:self.catalogueURLString];
     NSURLRequest *requestURL = [[NSURLRequest alloc] initWithURL:catalogueURL];
     [self.PDFViewer loadRequest:requestURL];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)loadingProgress
+{
+    // Creates and Starts Activity Indicator
+    self.loadingIndicator.hidesWhenStopped = YES;
+    [self.loadingIndicator startAnimating];
+    [self.view addSubview:self.loadingIndicator];
+    
 }
 
-- (void)intialiseWebViewer
+
+#pragma mark WKWebView Navigation Delegates
+
+- (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation
 {
-    CGRect webViewFrame = self.view.bounds;
-    self.PDFViewer = [[WKWebView alloc] initWithFrame:webViewFrame];
-    self.PDFViewer.UIDelegate = self;
-    [self.view addSubview:self.PDFViewer];
-    // Advice from: https://stackoverflow.com/questions/30737254/wkwebview-added-as-subview-is-not-resized-on-rotation-in-swift
-    [self.PDFViewer setAutoresizingMask:UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth];
-    
+    // Stop Animating Activity Indicator
+    [self.loadingIndicator stopAnimating];
 }
 
 
